3d2876b322aa77e0415186eb5e41e8d5b674c841
simon <fctinstean@gmail.com>
2020-10-26 16:04:37 +0100

    SQL File

--- PHP/Day 4 - Working With Database/moviedatabase.sql
+++ PHP/Day 4 - Working With Database/moviedatabase.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 07 juil. 2020 à 08:31
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.3.12
+-- Hôte : localhost:8889
+-- Généré le :  lun. 26 oct. 2020 à 15:04
+-- Version du serveur :  5.7.25
+-- Version de PHP :  7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,22 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `moviedatabase`
+-- Base de données :  `movie_db`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `directors`
---
-
-DROP TABLE IF EXISTS `directors`;
-CREATE TABLE IF NOT EXISTS `directors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `nationality` enum('USA','Luxembourg','France','Italy') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -42,15 +26,41 @@ CREATE TABLE IF NOT EXISTS `directors` (
 -- Structure de la table `movies`
 --
 
-DROP TABLE IF EXISTS `movies`;
-CREATE TABLE IF NOT EXISTS `movies` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
   `title` varchar(40) NOT NULL,
   `views` int(11) DEFAULT NULL,
-  `director_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `director_id` (`director_id`)
+  `director_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `movies`
+--
+
+INSERT INTO `movies` (`id`, `title`, `views`, `director_id`) VALUES
+(1, 'Fight Club', 12341234, 1),
+(2, 'Star Wars', 431234123, 2);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `director_id` (`director_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
@@ -61,7 +71,6 @@ CREATE TABLE IF NOT EXISTS `movies` (
 --
 ALTER TABLE `movies`
   ADD CONSTRAINT `movies_ibfk_1` FOREIGN KEY (`director_id`) REFERENCES `directors` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

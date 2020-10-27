62c2630707ae398d15e2202c2c034a3593d567c9
simon <fctinstean@gmail.com>
2020-10-27 12:06:31 +0100

    solution db

---
+++ PHP/Day 4 - Working With Database/Example Files/mypage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Page</title>
+</head>
+<body>
+    <h1>My new page</h1>
+
+    <?php
+        //var_dump($_GET);
+        //1. Get the id
+        $id = $_GET['id'];
+        //2. Run a query using that id
+
+        //3. Display the result of the query
+
+    ?>
+</body>
+</html>
---
+++ PHP/Day 4 - Working With Database/Exercises/Solutions/movies.php
@@ -0,0 +1,49 @@
+<?php
+// Retrieve all movies and display them : 
+
+// Require DB configuration
+require_once 'database.php';
+
+// Connect to DB
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
+$sql_query = 'SELECT * 
+            FROM movies';
+
+// Execute the query
+$results = mysqli_query($conn, $sql_query);
+
+// Fetch results as associative array
+$movies = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Website</title>
+</head>
+
+<body>
+    <h2>Movies list</h2>
+
+    <?php foreach ($movies as $movie) : ?>
+        <hr>
+        <p>
+            <strong>Title : </strong>
+            <?= $movie['title']; ?>
+        </p>
+        <p>
+            <strong>Views : </strong>
+            <?= $movie['views']; ?>
+        </p>
+
+        <img src="images/<?= $movie['poster'] ?>" width="100px">
+    <?php endforeach; ?>
+
+</body>
+
+</html>
